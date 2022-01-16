7903e51b594e3a7fafaefd957b94e903f836ecee
metrixsandbox <bradleylsaunders@gmail.com>
2022-01-16 18:44:04 -0500

    added second input

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,9 @@
     let generic;
 	let synonym;
 	let name;
+	let name2;
+	let suggest1;
+	let suggest2;
 
 	const nameFinder = () => {
 		fetch('https://rxnav.nlm.nih.gov/REST/drugs.json?name=' + name)
@@ -10,8 +13,19 @@
 			.then((drug) => {
 				generic = drug.drugGroup.conceptGroup[2].conceptProperties[0].name;
 				synonym = drug.drugGroup.conceptGroup[2].conceptProperties[0].synonym;
-			})
-		}
+			}) 	
+	}
+
+	const nameNotFound = () => {
+		fetch('https://rxnav.nlm.nih.gov/REST/spellingsuggestions.json?name=' + name2)
+			.then((response) => response.json())
+			.then((suggest) => {
+				suggest1 = suggest.suggestionGroup.suggestionList.suggestion[0]
+				suggest2 = suggest.suggestionGroup.suggestionList.suggestion[1]
+			}) 	
+	}
+
+
 </script>
 
 <main>
@@ -29,6 +43,21 @@
 	
 	<p>Generic: {generic}</p>
 	<p>Brand: {synonym} </p>
+
+	<h3>Did you misspell something? Try below.</h3>
+
+	<form on:submit|preventDefault={nameNotFound}>
+		
+		<input bind:value={name2}>
+		
+		<button>
+			Click me
+		</button>	
+			
+	</form>
+	
+	<p>Did you mean {suggest1} or {suggest2}?</p>
+	
 </main>
 
 <style>
@@ -44,6 +73,12 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	h3 {
+		color: #ff3e00;
+		font-size: 2em;
+		font-weight: 50;
 	}
 
 	@media (min-width: 640px) {
